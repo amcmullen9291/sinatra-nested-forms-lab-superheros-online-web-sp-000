421a4d53875ded7304b337bf6ee170c458db382a
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-29 19:40:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,20 +12,11 @@ class App < Sinatra::Base
   post '/teams' do
       @team = Team.new(params[:team])
       params[:team][:members].each do |details| 
-<<<<<<< HEAD
         binding.pry
         SuperHero.new(details)
       end
       @members = SuperHero.all
       erb :teams
     end  
-=======
-        SuperHero.new(details)
-      end
-      @members = SuperHero.all
-	binding.pry
-      erb :teams
 
-    end
->>>>>>> 5149b08f4412f6535c4e9ae2699f5958f3b780f9
 end
