d3abd7293f5a89db07d5460e08ebee294bf7f440
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-29 19:38:58 +0000

    Automatically backed up by Learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 class SuperHero 
+=======
+class SupeHero 
+>>>>>>> 5149b08f4412f6535c4e9ae2699f5958f3b780f9
   attr_accessor :name, :power, :bio
   @@all = []
   def initialize(params)
